335fdfef5fa9a04ee7b2a0d79adcc21b96e1120c
王凯文 <3089691792@qq.com>
2024-10-24 19:04:21 +0800

    我对以下方面进行了改进： 增加了更多的注释，解释了每个部分的作用。 将useAppContext方法名改为testApplicationContext，使其更具描述性。 在assertEquals方法中添加了清晰的错误消息，以便在测试失败时能够更容易地诊断问题。

--- app/src/androidTest/java/com/licheedev/serialtool/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/licheedev/serialtool/ExampleInstrumentedTest.java
@@ -7,20 +7,29 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
 /**
- * Instrumentation test, which will execute on an Android device.
+ * 仪器测试类，用于在Android设备上执行。
+ * 该测试类用于验证应用程序的上下文是否正确。
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="http://d.android.com/tools/testing">Android 测试文档</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.itdlc.coin", appContext.getPackageName());
+    /**
+     * 测试方法，用于验证应用程序的包名是否正确。
+     * 通过获取目标应用的上下文并与预期的包名进行比较。
+     *
+     * @throws Exception 如果测试过程中发生任何异常
+     */
+    @Test
+    public void testApplicationContext() throws Exception {
+        // 获取被测试应用的上下文
+        Context targetContext = InstrumentationRegistry.getTargetContext();
+
+        // 验证应用的包名是否符合预期
+        String expectedPackageName = "com.itdlc.coin";
+        String actualPackageName = targetContext.getPackageName();
+        assertEquals("应用的包名不符合预期", expectedPackageName, actualPackageName);
     }
 }
